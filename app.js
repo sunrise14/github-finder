d72778bd0a3d97ea969cf8bb36d4d98d18a43185
rishi <34141382+rishitreddy@users.noreply.github.com>
2018-01-20 19:59:03 -0500

    adding showProfile, showAlert, clearAlert methods

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 github = new Github();
+ui = new UI();
 document.getElementById("searchUser").addEventListener('keyup', (e)=>{
     const userText = e.target.value;
     if (userText !== ''){
         github.getUser(userText)
         .then(data => {
             if (data.profile.message === "Not Found"){
-                //show alert
+                ui.showAlert("User Not Found", "alert alert-danger");
             }
             else{
-                //show profile
+                ui.showProfile(data.profile);
             }
         })
     }
